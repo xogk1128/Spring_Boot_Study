4e5691a477e027d62f1900dd1775c49dbdaaab4d
Taeha Kim <xogk1128@naver.com>
2023-03-17 14:39:31 +0900

    Singleton Scope

--- core/src/test/java/hello/core/lifeCycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -2,6 +2,8 @@ package hello.core.lifeCycle;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -12,7 +14,8 @@ public class NetworkClient {
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        
+//        connect();
+//        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url){
@@ -46,4 +49,16 @@ public class NetworkClient {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
+
+//    @Override
+//    public void destroy() throws Exception {
+//        disconnect();
+//    }
+//
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
 }
+
---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,11 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+
+    }
+}
