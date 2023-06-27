34d9c43ff7bc1eba32522bfa830cd72cdba291ac
Taeha Kim <xogk1128@naver.com>
2023-06-27 12:57:58 +0900

    request Param Map

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -83,6 +84,14 @@ public class RequestParamController {
             @RequestParam(required = false, defaultValue = "-1") int age){
 
         log.info("username={}, age={}",username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap){
+
+        log.info("username={}, age={}",paramMap.get("username"), paramMap.get("age"));
         return "ok";
     }
 }
