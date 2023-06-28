eae220cce330d4f14b278230055c548a41d3bf6d
Taeha Kim <xogk1128@naver.com>
2023-06-28 14:40:57 +0900

    request body V2

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -9,6 +9,8 @@ import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
@@ -24,5 +26,12 @@ public class RequestBodyStringController {
         response.getWriter().write("ok");
     }
 
-    
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWrite) throws IOException {
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+        responseWrite.write("ok");
+    }
+
 }
