c8f26ec33487b98971bd58f024361f9fa2f19779
Taeha Kim <xogk1128@naver.com>
2023-07-25 12:41:48 +0900

    add Register Form

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -37,6 +35,33 @@ public class BasicItemController {
     @GetMapping("/add")
     public String addForm(){
         return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+//    public String addItemV1(@RequestParam String itemName,
+//                       @RequestParam int price,
+//                       @RequestParam Integer quantity,
+//                       Model model){
+//        Item item = new Item();
+//        item.setItemName(itemName);
+//        item.setPrice(price);
+//        item.setQuantity(quantity);
+//
+//        itemRepository.save(item);
+//
+//        model.addAttribute("item", item);
+//
+//        return "basic/item";
+//    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model){
+
+        itemRepository.save(item);
+        // 자동 추가, 생략 가능
+        //model.addAttribute("item", item);
+
+        return "basic/item";
     }
 
     // 테스트용 데이터 추가
