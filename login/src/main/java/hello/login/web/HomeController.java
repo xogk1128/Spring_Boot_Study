5bb6b90dbf98ec0a3de50d12d7532d073553e662
xogk1128 <xogk1128@naver.com>
2023-08-22 18:05:14 +0900

    use httpSession

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -42,18 +43,37 @@ public class HomeController {
 //        return "loginHome";
 //    }
 
+//    @GetMapping("/")
+//    public String homeLoginV2(HttpServletRequest request, Model model){
+//
+//        // 세션 관리자에 저장된 회원 정보 조회
+//        Member member = (Member) sessionManager.getSession(request);
+//
+//        // 로그인
+//        if (member == null){
+//            return "home";
+//        }
+//
+//        model.addAttribute("member", member);
+//        return "loginHome";
+//    }
+
     @GetMapping("/")
-    public String homeLoginV2(HttpServletRequest request, Model model){
+    public String homeLoginV3(HttpServletRequest request, Model model){
 
-        // 세션 관리자에 저장된 회원 정보 조회
-        Member member = (Member) sessionManager.getSession(request);
-
-        // 로그인
-        if (member == null){
+        HttpSession session = request.getSession(false);
+        if (session == null){
             return "home";
         }
 
-        model.addAttribute("member", member);
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
