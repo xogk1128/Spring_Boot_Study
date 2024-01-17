4fc045d611d0766881c2e9524c3ee01987abdc87
xogk1128 <xogk1128@naver.com>
2024-01-17 16:53:07 +0900

    키체인 오류 테스트

--- jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -32,7 +32,6 @@ public class Category {
     private List<Category> child = new ArrayList<>();
 
     // == 연관관계 메서드 == //
-    // 키체인 테스트
     public void addChildCategory(Category child){
         this.child.add(child);
         child.setParent(this);
