78a02a24709ddb9488db7539d6e60219fc3675e1
xogk1128 <xogk1128@naver.com>
2024-01-19 17:54:52 +0900

    변경 감지 및 병합

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,14 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item finditem = itemRepository.findOne(itemId);
+        finditem.setName(name);
+        finditem.setPrice(price);
+        finditem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems(){
