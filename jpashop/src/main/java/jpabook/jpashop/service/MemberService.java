c6cef81db9f13eeec7034e6c3ffdc50daff38619
xogk1128 <xogk1128@naver.com>
2024-02-05 18:16:56 +0900

    스프링 데이터 JPA를 사용하여 MemberRepository 변경하기

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -44,12 +44,12 @@ public class MemberService {
 
     // 단건 조회
     public Member findOne(Long memberId){
-        return memberRepository.findOne(memberId);
+        return memberRepository.findById(memberId).get();
     }
 
     @Transactional
     public void update(Long id, String name){
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
     }
 }
